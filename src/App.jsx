29e47fce1e879f8ad44ada8eac1ac043bb18a1d9
sujalcharati <sujalc024@gmail.com>
2024-10-26 21:25:58 +0530

    initial pos

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useRef, useState } from 'react';
+
+import Phaser from 'phaser';
+import { PhaserGame } from './game/PhaserGame';
+
+function App ()
+{
+    
+        
+    
+
+    
+    return (
+        <div id="app">
+       
+        </div>
+    )
+
+}
+export default App
